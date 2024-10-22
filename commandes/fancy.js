c0c8395fb80b487e1e8d69ae47477070090bfc10
mr Anyway <simonstephano328@gmail.com>
2024-10-22 23:26:09 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Baraka-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BARAKA-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 Anyway-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('ANYWAY-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
