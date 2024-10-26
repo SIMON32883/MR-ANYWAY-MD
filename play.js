f5c20cb115d30ac1769832a4aed30a7b54175309
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2024-10-27 00:51:12 +0300

    .

--- play.js
+++ play.js
@@ -1,2 +1,8 @@
-//Fuck You Buy Ibradam Adams//
+const = {`ibrahim adams`}
+const = Remote_from_ibrahim_adams
 
+{Blocked}={enter password}
+
+//play premium api in the link below
+
+(`https://api.ibrahimadams.us.kg`)
