b28c32c2b23c4828188278ad4b6bbb1a6e1ff44e
Anywaytech1 <simonstephano108@gmail.com>
2024-11-07 15:23:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,12 +46,12 @@ const date = moment().format('DD/MM/YYYY');
 ╭───────────────────❒
 │❒⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
 │❒⁠⁠⁠⁠ *DEV1* : *Mr anyway*
-│❒⁠⁠⁠⁠ *DEV2* : *Baraka Bega*
+│❒⁠⁠⁠⁠ *DEV2* : *Ibrahim adams*
 ⁠⁠⁠⁠╰───────────────────❒
   `;
     
 let menuMsg = `
-     *Anyway Tech*
+     *ANYWAYTECH1*
 
 ❒────────────────────❒`;
 
