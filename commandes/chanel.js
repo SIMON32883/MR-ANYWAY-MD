1d3441685531b954ef9d6cceba909821ad0b010d
mr Anyway <simonstephano328@gmail.com>
2024-12-03 10:31:58 +0300

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -4,10 +4,10 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *TIMNASA 😎_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let z = 'Salut je m\'appelle *ANYWAY 😎_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *Timnasa Tech🏹*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://whatsapp.com/channel/0029VagWQ255q08VTCRQKP09';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
